24784d9b935901494b27972494ab0762435d3e89
YAShiba1 <108460620+YAShiba1@users.noreply.github.com>
2023-02-24 21:28:20 +0400

    first

---
+++ SpawnEnemies/Assets/Scripts/RandomEnemySpawn.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class RandomEnemySpawn : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private Transform _point;
+
+    private Transform[] _points;
+    private int _currentPoint;
+
+    void Start()
+    {
+        _points = new Transform[_point.childCount];
+
+        for (int i = 0; i < _point.childCount; i++)
+        {
+            _points[i] = _point.GetChild(i);
+        }
+
+        StartCoroutine(SpawnEnemy());
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        var waitForTwoSeconds = new WaitForSeconds(2f);
+
+        for (int i = 0; i < _points.Length; i++)
+        {
+            yield return waitForTwoSeconds;
+
+            Instantiate(_enemy, _points[_currentPoint].position, Quaternion.identity);
+
+            _currentPoint++;
+
+            Debug.Log(Time.time + " Enemy spawn.");
+
+            if (_currentPoint >= _points.Length)
+            {
+                _currentPoint = 0;
+            }
+        }
+    }
+}
